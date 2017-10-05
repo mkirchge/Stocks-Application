c83ad959af45b79abfd38673316d43b5b6a1644e
Max Kirchgesner <mkirchge@uci.edu>
2017-10-04 19:23:41 -0700

    changed fonts and took out unnecessary code

--- mystocks/src/GUI.java
+++ mystocks/src/GUI.java
@@ -1,53 +1,31 @@
 /**
- * Created by maxkirchgesner on 4/18/17.
+ *    Created by maxkirchgesner on 4/18/17.
  *
  *    This is a personal project that allows me to call on any stock and
  *    displays information to me in the way I want to see it
- */
+ *
+ **/
 
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
-import java.awt.event.ActionListener;
 import java.awt.event.*;
-import org.apache.commons.io.IOUtils;
-
-// For API integration
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.URL;
-import java.nio.charset.Charset;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
-
 public class GUI {
-
     private static int tablelength = 5;
     private static int currentrow = 0;
-    private static StockList mystocks;
-    private static StockList recentstocks = new StockList();
     private static Object rowData[][] = new Object[tablelength][3];
-
-
-    public GUI(StockList sl){
-
-    }
 
     /**************************
         GUI for Application
      **************************/
     public GUI() {
-
         JComboBox combobox;
         AutoCompleteDecorator decorator;
         JFrame frame;
@@ -64,12 +42,9 @@ public class GUI {
             Menu bar for stock search
          *******************************/
         JMenuBar menu = new JMenuBar();
-
         JMenu menu1 = new JMenu("Menu");
         JMenu menu2 = new JMenu("Stock Search");
         JMenu menu3 = new JMenu("News");
-
-        // add each menu item to the menu
         menu.add(menu1);
         menu1.add(menu2);
         menu1.add(menu3);
@@ -77,15 +52,11 @@ public class GUI {
         /**********************************************
             Creating the frame for the application
          **********************************************/
-        mystocks = new StockList();
-        // size of nasdaw file is 3218
-//        String[] nasdaq_stocks = new String[3218];
-        int count = 0;
+        StockList mystocks = new StockList();
         combobox = new JComboBox();
+        combobox.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
         combobox.insertItemAt("Click on dropdown to select a stock", 0);
-        for (int i = 1; i < 3218; i++){
-            combobox.insertItemAt(mystocks.getStocks().get(i).getSymbol() + " -- " + mystocks.getStocks().get(i).getName().replace(" - Common Stock", ""), i);
-        }
+        for (int i = 1; i < 3218; i++){ combobox.insertItemAt(mystocks.getStocks().get(i).getSymbol() + " -- " + mystocks.getStocks().get(i).getName().replace(" - Common Stock", ""), i); }
         combobox.setSelectedIndex(0);
         combobox.setMaximumRowCount(10);
         combobox.addActionListener ((ActionEvent e) -> {
@@ -97,36 +68,29 @@ public class GUI {
             // parses the name of the company
             String name2 = name.substring(1);
             Stock temp_stock = loadStock(name2,temp);
-            if (currentrow == 4) {
-                currentrow = 0;
-            }
+            if (currentrow == 4) { currentrow = 0; }
             rowData[currentrow][0] = " "+temp_stock.getName();
             rowData[currentrow][1] = " "+temp_stock.getSymbol();
-            rowData[currentrow][2] = " "+temp_stock.getPrice();
-            currentrow++;
-            GUI g = new GUI(recentstocks);
+            rowData[currentrow++][2] = " "+temp_stock.getPrice();
         });
-
         AutoCompleteDecorator.decorate(combobox);
         frame.setSize(1200,800);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
-
         frame.add(combobox);
         frame.setVisible(true);
 
         /****************
             Stock Table
          ****************/
-
         Object columnNames[] = { "Company", "Stock Symbol", "Price"};
         JTable table = new JTable(rowData, columnNames);
         table.setRowHeight(table.getRowHeight()+10);
-        table.setFont(new Font("Arial Unicode MS", Font.BOLD, 20));
+        table.getTableHeader().setFont(new Font("Lucide Grande", Font.BOLD, 20));
+        table.setFont(new Font("Lucida Grande", Font.BOLD, 16));
         JScrollPane scrollPane = new JScrollPane(table);
         scrollPane.setPreferredSize(new Dimension(800,600));
-
         MatteBorder border = new MatteBorder(1, 1, 0, 0, Color.BLACK);
         table.setBorder(border);
 
@@ -136,24 +100,18 @@ public class GUI {
         JPanel stockPanel = new JPanel();
         stockPanel.setBackground(Color.GREEN);
         stockPanel.setPreferredSize(new Dimension(300, 400));
-
         JPanel infoPanel = new JPanel();
         infoPanel.setBackground(Color.WHITE);
-
 //        JPanel previewPanel = new JPanel();
 //        previewPanel.setBackground(Color.ORANGE);
 //        previewPanel.setPreferredSize(new Dimension(100, 300));
-//
 //        JPanel pointsPanel = new JPanel();
 //        pointsPanel.setBackground(Color.RED);
 //        pointsPanel.setPreferredSize(new Dimension(300, 100));
-
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
         infoPanel.setPreferredSize(new Dimension(200, 400));
 //        infoPanel.add(previewPanel);
 //        infoPanel.add(pointsPanel);
-
-        // Adding components to frame
         frame.setJMenuBar(menu);
         frame.getContentPane().add(Box.createRigidArea(new Dimension(750,20)));
         frame.getContentPane().add(scrollPane);
@@ -165,22 +123,17 @@ public class GUI {
         Gets stock requested in search bar
      ****************************************/
     public static Stock loadStock(String company_name, String ticker_symbol) {
-
         // Create a JSON object
         String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
         Stock stock = new Stock(company_name, ticker_symbol);
-        JSONArray array = new JSONArray();
-
+        JSONArray array;
         //Load data from Stocks API
         try {
             String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
-
             JSONObject json = new JSONObject(getJson);
-
             JSONObject j2 = json.getJSONObject("dataset");
             array = j2.getJSONArray("data");
             // format is: date, open, high, low, end-of-day
-            // right now it is only getting the most recent day of the specified stock
             for (int i = 0; i < 1; i++) {
                 JSONArray temparray = array.getJSONArray(i);
                 stock.setPrice(temparray.getString(4));
@@ -188,27 +141,11 @@ public class GUI {
         } catch (JSONException | IOException e) {
             System.out.println("Caught exception: " + e);
         }
-
-        // KEY: stock symbol
-        // VALUE: company name, price, percent change, 52-week high, 52-week low
         return stock;
-    }
-
-    /**************************************
-       Stocks to add on click in combobox
-     **************************************/
-    public void addStocks(){
-        // when clicked on, get info from click and add to combobox
-
-        // add eventlistener to main function below, then call addStocks()
     }
 
     /**********
         Main
      **********/
-    public static void main(String[] args) {
-        GUI gui = new GUI();
-
-
-    }
+    public static void main(String[] args) { GUI gui = new GUI(); }
 }
