4d84fcbe414fce7f3fcc5d2f4da322ca8b38a677
Maximillian Kirchgesner <mkirchge@uci.edu>
2018-01-28 23:44:38 -0800

    added StockLoader class

---
+++ mystocks/src/StockLoader.java
@@ -0,0 +1,126 @@
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by maxkirchgesner on 1/28/18.
+ */
+public class StockLoader {
+
+    public StockLoader(){}
+
+    public static ArrayList<Double> loadStock5Day(String company_name, String ticker_symbol) {
+        // Create a JSON object
+        String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
+        JSONArray array;
+        ArrayList<Double> prices = new ArrayList<>();
+        //Load data from Stocks API
+        try {
+            String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
+            JSONObject json = new JSONObject(getJson);
+            JSONObject j2 = json.getJSONObject("dataset");
+            array = j2.getJSONArray("data");
+            // format is: date, open, high, low, end-of-day
+            for (int i = 4; i >= 0; i--){
+                JSONArray temparray = array.getJSONArray(i);
+                prices.add(Double.parseDouble(temparray.get(4).toString()));
+            }
+        } catch (JSONException | IOException e) {
+            System.out.println("Caught exception: " + e);
+        }
+        return prices;
+    }
+
+    public static ArrayList<Double> loadStock10Day(String company_name, String ticker_symbol) {
+        // Create a JSON object
+        String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
+        JSONArray array;
+        ArrayList<Double> prices = new ArrayList<>();
+        //Load data from Stocks API
+        try {
+            String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
+            JSONObject json = new JSONObject(getJson);
+            JSONObject j2 = json.getJSONObject("dataset");
+            array = j2.getJSONArray("data");
+            // format is: date, open, high, low, end-of-day
+            for (int i = 9; i >= 0; i--){
+                JSONArray temparray = array.getJSONArray(i);
+                prices.add(Double.parseDouble(temparray.get(4).toString()));
+            }
+        } catch (JSONException | IOException e) {
+            System.out.println("Caught exception: " + e);
+        }
+        return prices;
+    }
+
+    public static ArrayList<Double> loadStock1Month(String company_name, String ticker_symbol) {
+        // Create a JSON object
+        String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
+        JSONArray array;
+        ArrayList<Double> prices = new ArrayList<>();
+        //Load data from Stocks API
+        try {
+            String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
+            JSONObject json = new JSONObject(getJson);
+            JSONObject j2 = json.getJSONObject("dataset");
+            array = j2.getJSONArray("data");
+            // format is: date, open, high, low, end-of-day
+            for (int i = 30; i >= 0; i--){
+                JSONArray temparray = array.getJSONArray(i);
+                prices.add(Double.parseDouble(temparray.get(4).toString()));
+            }
+        } catch (JSONException | IOException e) {
+            System.out.println("Caught exception: " + e);
+        }
+        return prices;
+    }
+
+    public static ArrayList<Double> loadStock1Year(String company_name, String ticker_symbol) {
+        // Create a JSON object
+        String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
+        JSONArray array;
+        ArrayList<Double> prices = new ArrayList<>();
+        //Load data from Stocks API
+        try {
+            String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
+            JSONObject json = new JSONObject(getJson);
+            JSONObject j2 = json.getJSONObject("dataset");
+            array = j2.getJSONArray("data");
+            // format is: date, open, high, low, end-of-day
+            for (int i = 364; i >= 0; i--){
+                JSONArray temparray = array.getJSONArray(i);
+                prices.add(Double.parseDouble(temparray.get(4).toString()));
+            }
+        } catch (JSONException | IOException e) {
+            System.out.println("Caught exception: " + e);
+        }
+        return prices;
+    }
+
+    public static ArrayList<Double> loadStock5Year(String company_name, String ticker_symbol) {
+        // Create a JSON object
+        String url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker_symbol + ".json?api_key=tDSfEvKgfs6q-4KhB7Nd";
+        JSONArray array;
+        ArrayList<Double> prices = new ArrayList<>();
+        //Load data from Stocks API
+        try {
+            String getJson = org.apache.commons.io.IOUtils.toString(new URL(url), "UTF-8");
+            JSONObject json = new JSONObject(getJson);
+            JSONObject j2 = json.getJSONObject("dataset");
+            array = j2.getJSONArray("data");
+            // format is: date, open, high, low, end-of-day
+            for (int i = 728; i >= 0; i--){
+                JSONArray temparray = array.getJSONArray(i);
+                prices.add(Double.parseDouble(temparray.get(4).toString()));
+            }
+        } catch (JSONException | IOException e) {
+            System.out.println("Caught exception: " + e);
+        }
+        return prices;
+    }
+
+}
